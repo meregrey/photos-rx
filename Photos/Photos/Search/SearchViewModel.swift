a417f7d5e517fac0005291ffe5658db1d97a48cb
meregrey <yeojinyoon.dev@gmail.com>
2022-08-17 18:23:41 +0900

    SearchViewModel에 페이징을 위한 스트림 및 오퍼레이터 체인 구현

--- Photos/Photos/Search/SearchViewModel.swift
+++ Photos/Photos/Search/SearchViewModel.swift
@@ -11,19 +11,35 @@ import RxSwift
 
 final class SearchViewModel {
     let searchTerm = BehaviorRelay<String>(value: "")
+    let shouldLoadMorePhotos = BehaviorRelay<Bool>(value: false)
     let photos = BehaviorRelay<[PhotoViewModel]>(value: [])
     
-    private let relay = PublishRelay<[PhotoViewModel]>()
+    private let photoViewModels = PublishRelay<[PhotoViewModel]>()
+    private let api: APIType
     
+    private var page = 0
     private var disposeBag = DisposeBag()
     
     init(api: APIType = API.shared) {
+        self.api = api
+        
         searchTerm
             .distinctUntilChanged()
-            .flatMap(api.searchPhotos)
+            .flatMap(searchPhotos)
             .flatMap(transform)
             .bind(to: photos)
             .disposed(by: disposeBag)
+        
+        shouldLoadMorePhotos
+            .distinctUntilChanged()
+            .filter { $0 }
+            .subscribe(onNext: loadMorePhotos)
+            .disposed(by: disposeBag)
+    }
+    
+    private func searchPhotos(for searchTerm: String) -> Observable<[Photo]> {
+        page = 1
+        return api.searchPhotos(query: searchTerm, page: page)
     }
     
     private func transform(_ photos: [Photo]) -> Observable<[PhotoViewModel]> {
@@ -31,10 +47,24 @@ final class SearchViewModel {
         
         _ = ImageLoader.shared.loadImages(from: urls)
             .subscribe(onCompleted: {
-                let photoViewModels = photos.map(PhotoViewModel.init)
-                self.relay.accept(photoViewModels)
+                if photos.count > 0 {
+                    var photoViewModels = self.photos.value
+                    photoViewModels.append(contentsOf: photos.map(PhotoViewModel.init))
+                    self.photoViewModels.accept(photoViewModels)
+                } else {
+                    self.photoViewModels.accept([])
+                }
             })
         
-        return relay.asObservable()
+        return photoViewModels.asObservable()
+    }
+    
+    private func loadMorePhotos(_: Bool) {
+        page += 1
+        
+        api.searchPhotos(query: searchTerm.value, page: page)
+            .flatMap(transform)
+            .bind(to: photos)
+            .disposed(by: disposeBag)
     }
 }
