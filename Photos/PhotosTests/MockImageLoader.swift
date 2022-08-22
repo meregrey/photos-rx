e69419c956868bfa2a8ad4807d297adbc2c43b76
meregrey <yeojinyoon.dev@gmail.com>
2022-08-22 14:34:35 +0900

    실제 이미지 로드 대신 UIImage 인스턴스를 포함한 Observable을 반환하는 MockImageLoader 구조체 정의

---
+++ Photos/PhotosTests/MockImageLoader.swift
@@ -0,0 +1,20 @@
+//
+//  MockImageLoader.swift
+//  PhotosTests
+//
+//  Created by Yeojin Yoon on 2022/08/19.
+//
+
+@testable import Photos
+import Foundation
+import RxSwift
+
+struct MockImageLoader: ImageLoadable {
+    func loadImages(from urls: [URL]) -> Observable<UIImage> {
+        return Observable.just(UIImage())
+    }
+    
+    func image(from url: URL) -> UIImage? {
+        return nil
+    }
+}
